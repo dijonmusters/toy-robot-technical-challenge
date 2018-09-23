6280e216ed453b0883ccf83cef36987c30cf1940
iamqaz <jonathon.d.meyers@gmail.com>
2018-09-23 20:52:37 +1000

    NEW: Initialise ToyRobot project and tests

---
+++ ToyRobot/Robot.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ToyRobotConsole
+{
+    public class Robot
+    {
+        public bool IsWorking()
+        {
+            return true;
+        }
+    }
+}
