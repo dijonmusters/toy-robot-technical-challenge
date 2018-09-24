f461ca45316f565ef9152f3748de5e5d45bd6f3b
iamqaz <jonathon.d.meyers@gmail.com>
2018-09-24 21:57:10 +1000

    TEST: Implement tests for robot

---
+++ ToyRobot.Tests/RobotMoveTests.cs
@@ -0,0 +1,176 @@
+using NUnit.Framework;
+using ToyRobotConsole;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private Game _game;
+        private Robot _robot;
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+            _robot = new Robot(_game);
+            _robot.Place(0, 0, Direction.NORTH);
+        }
+
+        [Test]
+        public void RobotMoves()
+        {
+            Cell locBefore = _robot.Location;
+            _robot.Move();
+            Cell locAfter = _robot.Location;
+            Assert.AreNotEqual(locBefore, locAfter, $"{locBefore} should not be the same as {locAfter}");
+        }
+
+        [Test]
+        public void RobotMovesNorth()
+        {
+            _robot.Move();
+            int expectedX = 0;
+            int expectedY = 1;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotMovesEast()
+        {
+            _robot.RotateRight();
+            _robot.Move();
+            int expectedX = 1;
+            int expectedY = 0;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotMovesSouth()
+        {
+            _robot.Place(2, 2, Direction.SOUTH);
+            _robot.Move();
+            int expectedX = 2;
+            int expectedY = 1;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotMovesWest()
+        {
+            _robot.Place(2,2,Direction.WEST);
+            _robot.Move();
+            int expectedX = 1;
+            int expectedY = 2;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotRemainsInBoundaryNorth()
+        {
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            int expectedX = 0;
+            int expectedY = 4;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+            _robot.Move();
+            actualX = _robot.Location.X;
+            actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotRemainsInBoundaryEast()
+        {
+            _robot.Place(0, 0, Direction.EAST)
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            int expectedX = 4;
+            int expectedY = 0;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+            _robot.Move();
+            actualX = _robot.Location.X;
+            actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotRemainsInBoundarySouth()
+        {
+            _robot.Place(0, 4, Direction.SOUTH)
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            int expectedX = 0;
+            int expectedY = 0;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+            _robot.Move();
+            actualX = _robot.Location.X;
+            actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotRemainsInBoundaryWest()
+        {
+            _robot.Place(4, 0, Direction.WEST)
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            _robot.Move();
+            int expectedX = 0;
+            int expectedY = 0;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+            _robot.Move();
+            actualX = _robot.Location.X;
+            actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+
+        [Test]
+        public void RobotRemainsInBoundaryExtreme()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                _robot.Move();
+            }
+            int expectedX = 0;
+            int expectedY = 4;
+            int actualX = _robot.Location.X;
+            int actualY = _robot.Location.Y;
+            Assert.AreEqual(expectedX, actualX, $"{expectedX} should be the same as {actualX}");
+            Assert.AreEqual(expectedY, actualY, $"{expectedY} should be the same as {actualY}");
+        }
+    }
+}
---
+++ ToyRobot.Tests/RobotPlaceTests.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using ToyRobotConsole;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private Game _game;
+        private Robot _robot;
+        [SetUp]
+        public void Setup()
+        {
+            _game = new Game();
+            _robot = new Robot(_game);
+        }
+
+        [Test]
+        public void InactiveRobot()
+        {
+            bool actual = _robot.IsReady();
+            Cell actualLocation = _robot.Location;
+            Assert.IsFalse(actual, $"{actual} should be False");
+            Assert.IsNull(actualLocation, $"{actual} should be Null");
+        }
+
+        [Test]
+        public void InactiveRobotIgnoresCommands()
+        {
+            _robot.Move();
+            Cell actual = _robot.Location();
+            Assert.IsNull(actual, $"{actual} should be Null");
+        }
+
+        [Test]
+        public void ActiveRobot()
+        {
+            Direction direction = Direction.NORTH;
+            _robot.Place(0, 0, direction);
+            bool actual = _robot.IsReady();
+            int expectedPosX = 0;
+            int expectedPosY = 0;
+            Direction expectedDir = Direction.NORTH;
+            int actualPosX = _robot.Location.X;
+            int actualPosY = _robot.Location.Y;
+            Direction actualDir = _robot.Direction;
+            Assert.IsTrue(actual, $"{actual} should be True");
+            Assert.AreEqual(actualPosX, expectedPosX, $"{actualPosX} should be the same as {expectedPosX}");
+            Assert.AreEqual(actualPosY, expectedPosY, $"{actualPosY} should be the same as {expectedPosY}");
+            Assert.AreEqual(actualDir, expectedDir, $"{actualDir} should be the same as {expectedDir}");
+        }
+
+        [Test]
+        public void InvalidPlaceRobotOutOfBounds()
+        {
+            Direction direction = Direction.NORTH;
+            _robot.Place(100, 100, direction);
+            bool actual = _robot.IsReady();
+            Assert.IsFalse(actual, $"{actual} should be False");
+            Assert.IsNull(_robot.Location. "Robot location should be null");
+            Assert.IsNull(_robot.Direction. "Robot direction should be null");
+        }
+
+        [Test]
+        public void InvalidPlaceRobotDirection()
+        {
+            Direction direction = Direction.SILLY;
+            _robot.Place(0, 0, direction);
+            bool actual = _robot.IsReady();
+            Assert.IsFalse(actual, $"{actual} should be False");
+            Assert.IsNull(_robot.Location. "Robot location should be null");
+            Assert.IsNull(_robot.Direction. "Robot direction should be null");
+        }
+
+        [Test]
+        public void ActiveRobotMoves()
+        {
+            Cell posBefore = _robot.Location;
+            Direction direction = Direction.NORTH;
+            _robot.Place(0, 0, direction);
+            Cell posAfter = _robot.Location;
+            Assert.AreNotSame(posBefore, posAfter, $"{posAfter} should not be the same as {posBefore}");
+        }
+    }
+}
