b6c8b0a40ab9532a36dc6fea5ca08e9b55bd6d02
iamqaz <jonathon.d.meyers@gmail.com>
2018-09-25 14:25:33 +1000

    DOC: Add XML documentation for user interface

--- ToyRobot/UserInterface.cs
+++ ToyRobot/UserInterface.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 
 namespace ToyRobotConsole
 {
+    /// <summary>The User Interface is responsible for all interactions with the user.</summary>
     public class UserInterface
     {
+        /// <summary>Private field for game.</summary>
         Game _game;
 
+        /// <summary>User Interface constructor</summary>
+        /// <param name="game">A game parameter to access game elements</param>
         public UserInterface(Game game)
         {
             _game = game;
         }
 
-        private string CellSymbol(Cell c)
+        /// <summary>Determines which character should be rendered from cell</summary>
+        /// <param name="cell">A cell parameter to determine which symbol is required</param>
+        /// <returns>A string value containing the cell's symbol</returns>
+        private string CellSymbol(Cell cell)
         {
             string grid = " ";
-            if (_game.Robot.IsLocated(c))
+            if (_game.Robot.IsLocated(cell))
                 grid += "@";
             else
                 grid += " ";
             return grid;
         }
 
+        /// <summary>Calculates the number of dash characters required for grid border</summary>
+        /// <returns>A string value containing the border characters</returns>
         private string GridBorder()
         {
             int width = _game.Map[_game.Map.Count - 1].X + 1;
@@ -33,6 +42,8 @@ namespace ToyRobotConsole
             return border;
         }
 
+        /// <summary>Calculates the entire grid world</summary>
+        /// <returns>A string value containing the entire grid</returns>
         private string BuildGrid()
         {
             string grid = "";
@@ -49,6 +60,7 @@ namespace ToyRobotConsole
             return grid;
         }
 
+        /// <summary>Prints the grid to the console</summary>
         public void PrintGrid()
         {
             string grid = GridBorder();
@@ -58,18 +70,23 @@ namespace ToyRobotConsole
             Console.WriteLine(grid);
         }
 
+        /// <summary>Asks the user a question and returns their response</summary>
+        /// <param name="question">A string parameter containing the question for the user</param>
+        /// <returns>A string value containing the user's response</returns>
         public string AskUser(string question)
         {
             Console.Write(question);
             return Console.ReadLine();
         }
 
+        /// <summary>Prints a message to the console with a consistent format</summary>
+        /// <param name="message">A string parameter containing the message to be printed to console</param>
         public void PrintMessage(string message)
         {
             string formattedMessage = message.Replace("\n", "\n# ");
-            Console.WriteLine("\n#########");
+            Console.WriteLine("\n###########");
             Console.WriteLine($"# {formattedMessage}");
-            Console.WriteLine("#########\n");
+            Console.WriteLine("###########\n");
         }
     }
 }
